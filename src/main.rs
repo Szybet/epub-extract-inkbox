18a0f7f815f622f53c3acd19e6839d81d051f20e
Szybet <53944559+Szybet@users.noreply.github.com>
2022-05-17 09:03:52 +0200

    optimise for disk space

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 /*
-author
-pub date
+author ---
+pub date ---
 cover page location ---
 book name ---
 */
@@ -9,19 +9,17 @@ use epub::doc::EpubDoc;
 use std::fs;
 use std::io::Write;
 
-use log::{debug, error, info, log_enabled, Level};
 use std::env;
 use std::process;
-use tinyjson::JsonValue;
 
 fn main() {
     // Set RUST_LOG=debug to get debug output
-    env_logger::init();
-    debug!("Starting");
+    //env_logger::init();
+    //debug!("Starting");
 
     let args: Vec<String> = env::args().collect();
     let args_len = env::args().len();
-    debug!("CLI arguments are: {:?}", args);
+    //debug!("CLI arguments are: {:?}", args);
     if &args[1] == "--help" || &args[1] == "help" || args_len < 1 {
         println!("First argument: Path to book | Second: Path to extracted ePUB cover (optional). Set RUST_LOG=debug to get debug output.");
         process::exit(1);
@@ -32,12 +30,12 @@ fn main() {
     let mut doc = EpubDoc::new(epub_file).unwrap();
 
     // Whole hashmap
-    let metadata_all = doc.metadata.clone();
-    debug!("Whole metadata of ePUB: {:?}", metadata_all);
+    //let metadata_all = doc.metadata.clone();
+    //debug!("Whole metadata of ePUB: {:?}", metadata_all);
 
     // Title:
     let title = doc.mdata("title").unwrap();
-    debug!("Title is: {}", title);
+    //debug!("Title is: {}", title);
 
     // Cover:
     if args_len > 2 {
@@ -46,19 +44,18 @@ fn main() {
         let f = fs::File::create(cover_path);
         let mut f = f.unwrap();
         let resp = f.write_all(&cover_data).unwrap();
-        debug!("Cover extraction done");
-    }
-    else {
-        debug!("Not extracting book cover since no path was provided");
+        //debug!("Cover extraction done");
+    } else {
+        //debug!("Not extracting book cover since no path was provided");
     }
 
     // Publishing date
     let publish_date = doc.mdata("date").unwrap();
-    debug!("Publishing date: {:?}", publish_date);
+    //debug!("Publishing date: {:?}", publish_date);
 
     // Author
     let author = doc.mdata("creator").unwrap();
-    debug!("Author: {:?}", author);
+    //debug!("Author: {:?}", author);
 
     let json = r#"{
     "author": "author_replace",
